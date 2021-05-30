b551b5b4c918d5edb75ef664e6ad7645ab549425
naimur4062 <naimur4062@gmail.com>
2021-05-31 00:03:19 +0800

    update

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,4 +1,4 @@
 import { createStore } from "redux";
 import cartReducers from "../reducers/cartReducers";
 
-export const store = createStore(cartReducers);
+export const store = createStore(cartReducers)
